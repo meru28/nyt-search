1b44300e01e3df75b9f13b49cbb4603a92656f68
muntasir2165 <muntasir2165@hotmail.com>
2018-11-02 21:52:42 -0400

    added front-end react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Results from "./pages/Results";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
 
 const App = () => (
   <Router>
     <div>
       <Nav />
       <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
+        <Route exact path="/" component={Search} />
+        <Route exact path="/articles" component={Results} />
+        <Route exact path="/articles/:id" component={Saved} />
         <Route component={NoMatch} />
       </Switch>
     </div>
