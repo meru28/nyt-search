e3f8715bf18b5fcf6124b72c6b6c9f00e9767f65
muntasir2165 <muntasir2165@hotmail.com>
2018-11-02 21:46:00 -0400

    added boilerplate react components

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets all articles
+  getArticles: function() {
+    return axios.get("/api/articles");
   },
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  getArticle: function(id) {
+    return axios.get("/api/articles/" + id);
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  deleteArticle: function(id) {
+    return axios.delete("/api/articles/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  // Saves an article to the database
+  saveArticle: function(articleData) {
+    return axios.post("/api/articles", articleData);
   }
 };
